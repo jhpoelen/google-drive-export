c5d908efa695437f8399f53815076158c37e6387
Jorrit Poelen <jhpoelen+git@jhpoelen.nl>
2023-06-16 09:31:49 -0500

    add initial track google slides script

---
+++ track-google-slides.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+#
+# Tracks pdf, odt, pptx and txt version of openly accessible slides in google drive
+#
+# expects a stream of slides ids (e.g., 134TrNhem_-fgXggbAWh0TyvHB7mxm7is2hMS9vIlfjU) on stdin
+#
+# Example:
+#
+# echo https://docs.google.com/presentation/d/134TrNhem_-fgXggbAWh0TyvHB7mxm7is2hMS9vIlfjU/edit#slide=id.p\
+# | bash track-google-presentation.sh
+#
+#
+
+# for google drive id syntax https://developers.google.com/sheets/api/guides/concepts
+grep -oE 'https://docs.google.com/[a-z]+/d/[a-zA-Z0-9_-]+'\
+ | awk '{ print $1 "/export/pdf\n" $1 "/export/pptx\n" $1 "/export/odp\n" $1 "/export/txt" }'
